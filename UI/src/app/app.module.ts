7b13d2f8479e4642645194a85d83a50e2d6093bc
Janick Koder <system@myshops.eu>
2021-07-13 22:04:37 +0200

    Serverintegration

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -5,16 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+"use strict";
+const express = require("express");
+const compression = require("compression")
+const path = require('path')
+const app = express();
+const _app_folder = 'UI/dist/AquarinoidUI';
+const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.use(compression());
+
+app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: _app_folder});
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(port, function () {
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + port);
 });
